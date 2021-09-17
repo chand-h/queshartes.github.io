9ee8f89c0699e0aa5d7355a25945b11779654bc0
queshartes <90875720+queshartes@users.noreply.github.com>
2021-09-17 16:12:44 -0500

    Update webgl.js

--- webgl/webgl.js
+++ webgl/webgl.js
@@ -1,4 +1,28 @@
+var vertexShaderText = 
+[
+'precision mediump float;',
+'',
+'attribute vert2 vertPosition',
+'',
+'void main()',
+'{',
+'  gl_Position = vec4(vertPosition, 0.0, 1.0);',
+'}' 
+].join('\n');
+
+var fragmentShaderText =
+[
+'precision mediump float',
+'',
+'void main()',
+'{',
+'   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);',
+'}'
+].join('\n');
+
 var InitDemo() = function () {
+  
+  // init
   var canvas = document.getElementById('game-surface');
   var gl = canvas.getContext('webgl');
   if (!gl) {
@@ -7,6 +31,40 @@ var InitDemo() = function () {
   }
   if (!gl) {
     alert = ('NO WEBGL ON YOUR BROWSER BROKE BOI');
+  }
+  
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  gl.viewport(0, 0, window.innerWidth, window.innerHeight);
+  
+  gl.clearColor(0.75, 0.85, 0.8, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  
+  // create shaders
+  var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+  
+  gl.shaderSource(vertexShader, vertexShaderText); // p1 = shader to source - p2 = shader source
+  gl.shaderSource(fragmentShader, fragmentShaderText);
+  
+  gl.compileShader(vertexShader);
+  // check for compilation errors
+  if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+    console.error('COMPILATION ERROR IN VERTEX SHADER!', gl.getShaderInfoLog(vertexShader));
+    return;
+  }
+  gl.compileShader(fragmentShader);
+  if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+    console.error('COMPILATION ERROR IN FRAGMENT SHADER!', gl.getShaderInfoLog(fragmentShader));
+    return;
+  }
+  
+  var program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    console.error('ERROR LINKING PROGRAM!', gl.getProgramInfoLog(program));
   }
   
 };
