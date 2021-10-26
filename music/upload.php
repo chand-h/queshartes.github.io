8757025c092c72656dc1ac2ecdfed214f4522ad2
queshartes <90875720+queshartes@users.noreply.github.com>
2021-10-25 23:34:07 -0500

    Update upload.php

--- music/upload.php
+++ music/upload.php
@@ -1,16 +1,30 @@
 <?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileupload"]["name"]);
-$uploadOk = 1;
-$soundFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// in bytes
-if ($_FILES["fileToUpload"]["size"] > 100000000) {
-  echo "< 100MB";
-  $uploadOk = 0;
-} else if($imageFileType != "wav" && $imageFileType != "mp3") {
-  echo ".mp3 or .wav";
-  $uploadOk = 0;
-} else {
+  $target_dir = "uploads/";
+  $target_file = $target_dir . basename($_FILES["fileupload"]["name"]);
   $uploadOk = 1;
-}
+  $soundFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+  // Check if file already exists
+  if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+  }
+  // in bytes
+  if ($_FILES["fileToUpload"]["size"] > 100000000) {
+    echo "< 100MB";
+    $uploadOk = 0;
+  } 
+  if($imageFileType != "wav" && $imageFileType != "mp3") {
+    echo ".mp3 or .wav";
+    $uploadOk = 0;
+  }
+  if ($uploadOk == 0) {
+    echo "Error with file";
+  // if everything is ok, try to upload file
+  } else {
+    if (move_uploaded_file($_FILES["fileupload"]["tmp_name"], $target_file)) {
+      echo "The file ". htmlspecialchars( basename( $_FILES["fileupload"]["name"])). " has been uploaded.";
+    } else {
+      echo "Error uploading file";
+    }
+  }
 ?>
